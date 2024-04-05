b37866aad225641c107be4eb65e43c87fefc5351
Yasin Ertekin <yasinertekin24@gmail.com>
2024-04-05 16:43:53 +0300

    Refactor OnboardingView to use _OnboardingBuilder and add custom page selector

--- lib/feature/onboarding/view/onboarding_view.dart
+++ lib/feature/onboarding/view/onboarding_view.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:gen/gen.dart';
 import 'package:moonx/product/core/model/onboarding.dart';
+
+part 'mixin/onboarding_builder_mixin.dart';
+part 'widget/birth_date_view.dart';
+part 'widget/birth_place_view.dart';
+part 'widget/welcome_view.dart';
 
 /// Onboarding View
 final class OnboardingView extends StatelessWidget {
@@ -8,22 +14,57 @@ final class OnboardingView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final onboardingList = Onboarding.onboardingList;
-    return PageView.builder(
-      itemCount: onboardingList.length,
-      itemBuilder: (context, index) {
-        return Column(
-          children: [
-            Image.asset(
-              onboardingList[index].imageUrl,
-              width: 300,
-              height: 300,
-            ),
-            Text(onboardingList[index].title ?? ''),
-            Text(onboardingList[index].description),
-          ],
-        );
-      },
+    return const Scaffold(
+      backgroundColor: Colors.black,
+      body: _OnboardingBuilder(),
+    );
+  }
+}
+
+final class _OnboardingBuilder extends StatefulWidget {
+  const _OnboardingBuilder();
+
+  @override
+  State<_OnboardingBuilder> createState() => _OnboardingBuilderState();
+}
+
+final class _OnboardingBuilderState extends State<_OnboardingBuilder>
+    with _OnboardingBuilderMixin, SingleTickerProviderStateMixin {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Expanded(
+          child: PageView.builder(
+            physics: const NeverScrollableScrollPhysics(),
+            itemCount: OnBoardingEnum.values.length,
+            itemBuilder: onboardingItemBuilder,
+          ),
+        ),
+        _CustomPageSelector(controller: _controller),
+        ElevatedButton(
+          onPressed: () {},
+          child: const Text('Next'),
+        ),
+      ],
+    );
+  }
+}
+
+final class _CustomPageSelector extends StatelessWidget {
+  const _CustomPageSelector({
+    required TabController controller,
+  }) : _controller = controller;
+
+  final TabController _controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return TabPageSelector(
+      controller: _controller,
+      indicatorSize: 10,
+      color: Colors.grey,
+      selectedColor: Colors.white,
     );
   }
 }
