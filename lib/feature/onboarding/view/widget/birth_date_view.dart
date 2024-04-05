7b4fe1b6e8a2e29b385617dd4fc22c4dc934d173
Yasin Ertekin <yasinertekin24@gmail.com>
2024-04-05 16:44:11 +0300

    Add BirthDateView widget to onboarding feature

---
+++ lib/feature/onboarding/view/widget/birth_date_view.dart
@@ -0,0 +1,10 @@
+part of '../onboarding_view.dart';
+
+final class _BirthDateView extends StatelessWidget {
+  const _BirthDateView();
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column();
+  }
+}
