0a37a7b40fa79e769cedbd8b22954a0c6cc17583
Yasin Ertekin <yasinertekinn@outlook.com>
2024-04-10 22:46:24 +0300

    Refactor OnboardingCubit to use Equatable, add TabController initialization, and update nextPage method

--- lib/feature/onboarding/cubit/onboarding_cubit.dart
+++ lib/feature/onboarding/cubit/onboarding_cubit.dart
@@ -1,29 +1,59 @@
-import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:moonx/feature/onboarding/view/onboarding_view.dart';
 
-/// Onboarding Cubit
-final class OnboardingCubit extends Cubit<OnBoardingEnum> {
+part 'onboarding_state.dart';
+
+/// Onboarding OnboardingCubit
+final class OnboardingCubit extends Cubit<OnboardingState> {
   /// Constructor
-  OnboardingCubit() : super(OnBoardingEnum.welcome);
+  OnboardingCubit() : super(const OnboardingState());
 
-  /// Next
-  void next() {
-    if (state == OnBoardingEnum.birthPlace) {
-      return;
+  /// Tab Controller
+  late final TabController _tabController;
+
+  /// Initialize TabController
+  Future<void> initializeTabController(int length, TickerProvider vsync) async {
+    try {
+      _tabController = TabController(length: length, vsync: vsync);
+      emit(
+        state.copyWith(
+          tabController: _tabController,
+          isLoaded: true,
+        ),
+      );
+    } catch (e) {
+      emit(
+        state.copyWith(
+          error: e.toString(),
+        ),
+      );
     }
-    emit(OnBoardingEnum.values[state.index + 1]);
   }
 
-  /// Previous
-  void previous() {
-    if (state == OnBoardingEnum.welcome) {
-      return;
-    }
-    emit(OnBoardingEnum.values[state.index - 1]);
+  /// Dispose
+  void dispose() {
+    _tabController.dispose();
   }
 
-  /// Finish
-  void finish() {
-    emit(OnBoardingEnum.birthPlace);
+  /// Next Page
+  Future<void> nextPage() async {
+    if (state.tabController!.index < OnBoardingEnum.values.length - 1) {
+      state.tabController!.animateTo(
+        state.tabController!.index + 1,
+        duration: const Duration(
+          milliseconds: 500,
+        ),
+        curve: Curves.easeInOut,
+      );
+
+      emit(
+        state.copyWith(
+          tabController: state.tabController,
+          onBoardingEnum: OnBoardingEnum.values[state.tabController!.index],
+        ),
+      );
+    }
   }
 }
