ef5f8a6b9fad2e8948bea86be1473710d2e67249
Yasin Ertekin <yasinertekinn@outlook.com>
2024-04-10 22:46:48 +0300

    Refactor OnboardingView to import necessary packages and add missing widgets

--- lib/feature/onboarding/view/onboarding_view.dart
+++ lib/feature/onboarding/view/onboarding_view.dart
@@ -1,11 +1,40 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:gen/gen.dart';
-import 'package:moonx/product/core/model/onboarding.dart';
+import 'package:intl/intl.dart';
+import 'package:moonx/feature/home/view/home_view.dart';
+import 'package:moonx/feature/onboarding/cubit/onboarding_cubit.dart';
+import 'package:moonx/product/core/enum/project_padding.dart';
+import 'package:moonx/product/core/extension/build_context_extension.dart';
+import 'package:moonx/product/core/extension/scaffold_messenger_extension.dart';
+import 'package:moonx/product/utils/cache/users_bloc.dart';
+import 'package:moonx/product/utils/cache/users_event.dart';
+import 'package:moonx/product/utils/cache/users_state.dart';
+import 'package:moonx/product/widget/button/cancel_button.dart';
+import 'package:moonx/product/widget/button/done_button.dart';
+import 'package:moonx/product/widget/button/project_button.dart';
+import 'package:moonx/product/widget/container/empty_sized_box.dart';
 
+part 'mixin/birth_date_body_mixin.dart';
 part 'mixin/onboarding_builder_mixin.dart';
-part 'widget/birth_date_view.dart';
-part 'widget/birth_place_view.dart';
-part 'widget/welcome_view.dart';
+part 'mixin/project_button_mixin.dart';
+part 'mixin/select_birth_place_button_mixin.dart';
+part 'widget/birth_date_view/birth_date_view.dart';
+part 'widget/birth_date_view/widget/birth_date_description.dart';
+part 'widget/birth_date_view/widget/birth_date_view_background_image.dart';
+part 'widget/birth_date_view/widget/birth_time_text.dart';
+part 'widget/birth_date_view/widget/date_selector.dart';
+part 'widget/birth_date_view/widget/enter_birth_date_text.dart';
+part 'widget/birth_date_view/widget/select_birth_date_button.dart';
+part 'widget/birth_date_view/widget/select_birth_time_button.dart';
+part 'widget/birth_place_view/birth_place_view.dart';
+part 'widget/birth_place_view/widget/birth_place_background_image.dart';
+part 'widget/birth_place_view/widget/custom_city_picker.dart';
+part 'widget/birth_place_view/widget/select_birth_place_button.dart';
+part 'widget/custom_tab_page_selector.dart';
+part 'widget/onboarding_button.dart';
+part 'widget/welcome_view/welcome_view.dart';
 
 /// Onboarding View
 final class OnboardingView extends StatelessWidget {
@@ -14,9 +43,12 @@ final class OnboardingView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Colors.black,
-      body: _OnboardingBuilder(),
+    return BlocProvider<OnboardingCubit>(
+      create: (context) => OnboardingCubit(),
+      child: const Scaffold(
+        backgroundColor: Colors.black,
+        body: _OnboardingBuilder(),
+      ),
     );
   }
 }
@@ -32,39 +64,53 @@ final class _OnboardingBuilderState extends State<_OnboardingBuilder>
     with _OnboardingBuilderMixin, SingleTickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Expanded(
-          child: PageView.builder(
-            physics: const NeverScrollableScrollPhysics(),
-            itemCount: OnBoardingEnum.values.length,
-            itemBuilder: onboardingItemBuilder,
-          ),
-        ),
-        _CustomPageSelector(controller: _controller),
-        ElevatedButton(
-          onPressed: () {},
-          child: const Text('Next'),
-        ),
-      ],
+    return BlocBuilder<OnboardingCubit, OnboardingState>(
+      builder: (context, state) {
+        if (state.isLoaded) {
+          return Column(
+            children: [
+              Expanded(
+                child: TabBarView(
+                  controller: state.tabController,
+                  physics: const NeverScrollableScrollPhysics(),
+                  children: List.generate(
+                    OnBoardingEnum.values.length,
+                    (index) => onboardingItemBuilder(context, index),
+                  ),
+                ),
+              ),
+              _CustomTabPageSelector(
+                controller: state.tabController!,
+              ),
+              const _OnboardingButton(),
+            ],
+          );
+        }
+        if (state.error.isNotEmpty) {
+          return _ErrorText(
+            error: state.error,
+          );
+        }
+        return const EmptySizedBox();
+      },
     );
   }
 }
 
-final class _CustomPageSelector extends StatelessWidget {
-  const _CustomPageSelector({
-    required TabController controller,
-  }) : _controller = controller;
+final class _ErrorText extends StatelessWidget {
+  const _ErrorText({
+    required this.error,
+  });
 
-  final TabController _controller;
+  final String error;
 
   @override
   Widget build(BuildContext context) {
-    return TabPageSelector(
-      controller: _controller,
-      indicatorSize: 10,
-      color: Colors.grey,
-      selectedColor: Colors.white,
+    return Center(
+      child: Text(
+        error,
+        style: context.theme.textTheme.bodyLarge,
+      ),
     );
   }
 }
