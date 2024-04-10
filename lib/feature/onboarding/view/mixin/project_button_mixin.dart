e40c4db9ef7496fb708951c37ed26aae21c82e0f
Yasin Ertekin <yasinertekinn@outlook.com>
2024-04-10 22:47:23 +0300

    Refactor OnboardingView to use mixin for handling onboarding actions

---
+++ lib/feature/onboarding/view/mixin/project_button_mixin.dart
@@ -0,0 +1,33 @@
+part of '../onboarding_view.dart';
+
+base mixin _ProjectButtonMixin on State<_OnboardingButton> {
+  void handleOnboardingAction(
+    OnBoardingEnum onboardingState,
+    String birthDate,
+    String birthTime,
+    String placeOfBirth,
+  ) {
+    switch (onboardingState) {
+      case OnBoardingEnum.welcome:
+        context.read<OnboardingCubit>().nextPage();
+      case OnBoardingEnum.birthDate:
+        if (birthDate.isEmpty || birthTime.isEmpty) {
+          context.showSnackBar(
+            const SnackBar(
+              content: Text('Please select birth date and time'),
+            ),
+          );
+          return;
+        }
+        context.read<OnboardingCubit>().nextPage();
+      case OnBoardingEnum.birthPlace:
+        if (placeOfBirth.isEmpty) return;
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (_) => const HomeView(),
+          ),
+        );
+    }
+  }
+}
