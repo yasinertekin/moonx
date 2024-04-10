7f945b1d8fae8d10aac9673036de77608c2d6eb6
Yasin Ertekin <yasinertekinn@outlook.com>
2024-04-10 22:47:33 +0300

    Add mixin for selecting birth place in OnboardingView

---
+++ lib/feature/onboarding/view/mixin/select_birth_place_button_mixin.dart
@@ -0,0 +1,18 @@
+part of '../onboarding_view.dart';
+
+base mixin _SelectBirthPlaceMixin on State<_SelectBirthPlaceButton> {
+  final _cityList = [
+    'Dhaka',
+    'Chittagong',
+    'Khulna',
+    'Rajshahi',
+    'Sylhet',
+    'Barisal',
+    'Rangpur',
+    'Mymensingh',
+    'İstanbul',
+  ];
+
+  /// Show city picker
+  void _showCityPicker(BuildContext context) {}
+}
