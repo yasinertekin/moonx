6a3e3ae6f8c74cb63f6f2cd95e3a47dd7e3a6776
Yasin Ertekin <yasinertekinn@outlook.com>
2024-04-11 10:54:13 +0300

    Add StringConstants class to store string constants

---
+++ lib/product/core/constants/string_constants.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+@immutable
+
+/// String Constants
+final class StringConstants {
+  const StringConstants._();
+
+  /// Project Button
+  static const String next = 'Next';
+}
