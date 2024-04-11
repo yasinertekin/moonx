def853050e6f15d51087f0f5e475e8e3388f0eb8
Yasin Ertekin <yasinertekinn@outlook.com>
2024-04-11 10:55:28 +0300

    Add ProjectPadding enum to store padding values

---
+++ lib/product/core/enum/project_padding.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+/// Project Padding
+enum ProjectPadding {
+  /// Low padding 8 px
+  low(8),
+
+  /// Medium padding 16 px
+  medium(16),
+
+  /// High padding 24 px
+  high(24);
+
+  /// Padding value
+  final double _value;
+
+  /// Constructor
+  // ignore: sort_constructors_first
+  const ProjectPadding(this._value);
+
+  /// Padding all
+  EdgeInsets get _paddingAll => EdgeInsets.all(_value);
+
+  /// Padding All Getter
+  EdgeInsets get paddingAll => _paddingAll;
+
+  /// Padding horizontal
+  EdgeInsets get _paddingHorizontal => EdgeInsets.symmetric(horizontal: _value);
+
+  /// Padding Horizontal Getter
+  EdgeInsets get paddingHorizontal => _paddingHorizontal;
+
+  /// Padding vertical
+  EdgeInsets get _paddingVertical => EdgeInsets.symmetric(vertical: _value);
+
+  /// Padding Vertical Getter
+  EdgeInsets get paddingVertical => _paddingVertical;
+
+  /// Padding only Left
+  EdgeInsetsGeometry get _paddingOnlyLeft => EdgeInsets.only(left: _value);
+
+  /// Padding Only Left Getter
+  EdgeInsetsGeometry get paddingOnly => _paddingOnlyLeft;
+
+  /// Padding only right
+  EdgeInsetsGeometry get _paddingOnlyRight => EdgeInsets.only(right: _value);
+
+  /// Padding Only Right Getter
+  EdgeInsetsGeometry get paddingOnlyRight => _paddingOnlyRight;
+}
