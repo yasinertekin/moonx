4a28b39aadbfa203812a34a02d746571cdab5a9d
Yasin Ertekin <yasinertekinn@outlook.com>
2024-04-11 10:56:01 +0300

    Add ProjectInitializer class to initialize the project

---
+++ lib/product/core/initialize/project_initializer.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:path_provider/path_provider.dart';
+
+@immutable
+
+/// Initialize
+final class ProjectInitializer {
+  const ProjectInitializer._();
+
+  /// Initialize
+  static Future<void> initialize() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    HydratedBloc.storage = await HydratedStorage.build(
+      storageDirectory: kIsWeb
+          ? HydratedStorage.webStorageDirectory
+          : await getApplicationDocumentsDirectory(),
+    );
+  }
+}
