d2c3d9e1e448a1ad8924a7ad5826223eb7bd740d
Yasin Ertekin <yasinertekinn@outlook.com>
2024-04-13 12:17:28 +0300

    Add MeditationView widget for the meditation feature

---
+++ lib/feature/meditation/view/meditation_view.dart
@@ -0,0 +1,12 @@
+import 'package:auto_route/annotations.dart';
+import 'package:flutter/material.dart';
+
+@RoutePage()
+final class MeditationView extends StatelessWidget {
+  const MeditationView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
