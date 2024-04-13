c57922be393ecb2960b614015a229af746beb334
Yasin Ertekin <yasinertekinn@outlook.com>
2024-04-13 12:24:36 +0300

    Refactor app_router.gr.dart to use auto_route.dart instead of moonx package

--- lib/product/utils/router/app_router.gr.dart
+++ lib/product/utils/router/app_router.gr.dart
@@ -8,83 +8,62 @@
 // coverage:ignore-file
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
-import 'package:auto_route/auto_route.dart' as _i8;
-import 'package:moonx/feature/deneme/deneme_view.dart' as _i1;
-import 'package:moonx/feature/home/view/home_view.dart' as _i2;
-import 'package:moonx/feature/meditation/view/meditation_view.dart' as _i3;
-import 'package:moonx/feature/nav_bar/nav_bar_view.dart' as _i4;
-import 'package:moonx/feature/onboarding/view/onboarding_view.dart' as _i5;
-import 'package:moonx/feature/profile/view/profile_view.dart' as _i6;
-import 'package:moonx/feature/splash/view/splash_view.dart' as _i7;
+import 'package:auto_route/auto_route.dart' as _i7;
+import 'package:moonx/feature/home/view/home_view.dart' as _i1;
+import 'package:moonx/feature/meditation/view/meditation_view.dart' as _i2;
+import 'package:moonx/feature/nav_bar/nav_bar_view.dart' as _i3;
+import 'package:moonx/feature/onboarding/view/onboarding_view.dart' as _i4;
+import 'package:moonx/feature/profile/view/profile_view.dart' as _i5;
+import 'package:moonx/feature/splash/view/splash_view.dart' as _i6;
 
-abstract class $AppRouter extends _i8.RootStackRouter {
+abstract class $AppRouter extends _i7.RootStackRouter {
   $AppRouter({super.navigatorKey});
 
   @override
-  final Map<String, _i8.PageFactory> pagesMap = {
-    DenemeRoute.name: (routeData) {
-      return _i8.AutoRoutePage<dynamic>(
-        routeData: routeData,
-        child: const _i1.DenemeView(),
-      );
-    },
+  final Map<String, _i7.PageFactory> pagesMap = {
     HomeRoute.name: (routeData) {
-      return _i8.AutoRoutePage<dynamic>(
+      return _i7.AutoRoutePage<dynamic>(
         routeData: routeData,
-        child: const _i2.HomeView(),
+        child: const _i1.HomeView(),
       );
     },
     MeditationRoute.name: (routeData) {
-      return _i8.AutoRoutePage<dynamic>(
+      return _i7.AutoRoutePage<dynamic>(
         routeData: routeData,
-        child: const _i3.MeditationView(),
+        child: const _i2.MeditationView(),
       );
     },
     NavBarRoute.name: (routeData) {
-      return _i8.AutoRoutePage<dynamic>(
+      return _i7.AutoRoutePage<dynamic>(
         routeData: routeData,
-        child: const _i4.NavBarView(),
+        child: const _i3.NavBarView(),
       );
     },
     OnboardingRoute.name: (routeData) {
-      return _i8.AutoRoutePage<dynamic>(
+      return _i7.AutoRoutePage<dynamic>(
         routeData: routeData,
-        child: const _i5.OnboardingView(),
+        child: const _i4.OnboardingView(),
       );
     },
     ProfileRoute.name: (routeData) {
-      return _i8.AutoRoutePage<dynamic>(
+      return _i7.AutoRoutePage<dynamic>(
         routeData: routeData,
-        child: const _i6.ProfileView(),
+        child: const _i5.ProfileView(),
       );
     },
     SplashRoute.name: (routeData) {
-      return _i8.AutoRoutePage<dynamic>(
+      return _i7.AutoRoutePage<dynamic>(
         routeData: routeData,
-        child: const _i7.SplashView(),
+        child: const _i6.SplashView(),
       );
     },
   };
 }
 
 /// generated route for
-/// [_i1.DenemeView]
-class DenemeRoute extends _i8.PageRouteInfo<void> {
-  const DenemeRoute({List<_i8.PageRouteInfo>? children})
-      : super(
-          DenemeRoute.name,
-          initialChildren: children,
-        );
-
-  static const String name = 'DenemeRoute';
-
-  static const _i8.PageInfo<void> page = _i8.PageInfo<void>(name);
-}
-
-/// generated route for
-/// [_i2.HomeView]
-class HomeRoute extends _i8.PageRouteInfo<void> {
-  const HomeRoute({List<_i8.PageRouteInfo>? children})
+/// [_i1.HomeView]
+class HomeRoute extends _i7.PageRouteInfo<void> {
+  const HomeRoute({List<_i7.PageRouteInfo>? children})
       : super(
           HomeRoute.name,
           initialChildren: children,
@@ -92,13 +71,13 @@ class HomeRoute extends _i8.PageRouteInfo<void> {
 
   static const String name = 'HomeRoute';
 
-  static const _i8.PageInfo<void> page = _i8.PageInfo<void>(name);
+  static const _i7.PageInfo<void> page = _i7.PageInfo<void>(name);
 }
 
 /// generated route for
-/// [_i3.MeditationView]
-class MeditationRoute extends _i8.PageRouteInfo<void> {
-  const MeditationRoute({List<_i8.PageRouteInfo>? children})
+/// [_i2.MeditationView]
+class MeditationRoute extends _i7.PageRouteInfo<void> {
+  const MeditationRoute({List<_i7.PageRouteInfo>? children})
       : super(
           MeditationRoute.name,
           initialChildren: children,
@@ -106,13 +85,13 @@ class MeditationRoute extends _i8.PageRouteInfo<void> {
 
   static const String name = 'MeditationRoute';
 
-  static const _i8.PageInfo<void> page = _i8.PageInfo<void>(name);
+  static const _i7.PageInfo<void> page = _i7.PageInfo<void>(name);
 }
 
 /// generated route for
-/// [_i4.NavBarView]
-class NavBarRoute extends _i8.PageRouteInfo<void> {
-  const NavBarRoute({List<_i8.PageRouteInfo>? children})
+/// [_i3.NavBarView]
+class NavBarRoute extends _i7.PageRouteInfo<void> {
+  const NavBarRoute({List<_i7.PageRouteInfo>? children})
       : super(
           NavBarRoute.name,
           initialChildren: children,
@@ -120,13 +99,13 @@ class NavBarRoute extends _i8.PageRouteInfo<void> {
 
   static const String name = 'NavBarRoute';
 
-  static const _i8.PageInfo<void> page = _i8.PageInfo<void>(name);
+  static const _i7.PageInfo<void> page = _i7.PageInfo<void>(name);
 }
 
 /// generated route for
-/// [_i5.OnboardingView]
-class OnboardingRoute extends _i8.PageRouteInfo<void> {
-  const OnboardingRoute({List<_i8.PageRouteInfo>? children})
+/// [_i4.OnboardingView]
+class OnboardingRoute extends _i7.PageRouteInfo<void> {
+  const OnboardingRoute({List<_i7.PageRouteInfo>? children})
       : super(
           OnboardingRoute.name,
           initialChildren: children,
@@ -134,13 +113,13 @@ class OnboardingRoute extends _i8.PageRouteInfo<void> {
 
   static const String name = 'OnboardingRoute';
 
-  static const _i8.PageInfo<void> page = _i8.PageInfo<void>(name);
+  static const _i7.PageInfo<void> page = _i7.PageInfo<void>(name);
 }
 
 /// generated route for
-/// [_i6.ProfileView]
-class ProfileRoute extends _i8.PageRouteInfo<void> {
-  const ProfileRoute({List<_i8.PageRouteInfo>? children})
+/// [_i5.ProfileView]
+class ProfileRoute extends _i7.PageRouteInfo<void> {
+  const ProfileRoute({List<_i7.PageRouteInfo>? children})
       : super(
           ProfileRoute.name,
           initialChildren: children,
@@ -148,13 +127,13 @@ class ProfileRoute extends _i8.PageRouteInfo<void> {
 
   static const String name = 'ProfileRoute';
 
-  static const _i8.PageInfo<void> page = _i8.PageInfo<void>(name);
+  static const _i7.PageInfo<void> page = _i7.PageInfo<void>(name);
 }
 
 /// generated route for
-/// [_i7.SplashView]
-class SplashRoute extends _i8.PageRouteInfo<void> {
-  const SplashRoute({List<_i8.PageRouteInfo>? children})
+/// [_i6.SplashView]
+class SplashRoute extends _i7.PageRouteInfo<void> {
+  const SplashRoute({List<_i7.PageRouteInfo>? children})
       : super(
           SplashRoute.name,
           initialChildren: children,
@@ -162,5 +141,5 @@ class SplashRoute extends _i8.PageRouteInfo<void> {
 
   static const String name = 'SplashRoute';
 
-  static const _i8.PageInfo<void> page = _i8.PageInfo<void>(name);
+  static const _i7.PageInfo<void> page = _i7.PageInfo<void>(name);
 }
