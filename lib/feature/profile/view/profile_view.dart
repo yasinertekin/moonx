03f705e3ba3dd0ff3e8f83c1ed9ed323e61923fc
Yasin Ertekin <yasinertekinn@outlook.com>
2024-04-13 12:18:18 +0300

    Add ProfileView widget for the profile feature

---
+++ lib/feature/profile/view/profile_view.dart
@@ -0,0 +1,12 @@
+import 'package:auto_route/annotations.dart';
+import 'package:flutter/material.dart';
+
+@RoutePage()
+final class ProfileView extends StatelessWidget {
+  const ProfileView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
