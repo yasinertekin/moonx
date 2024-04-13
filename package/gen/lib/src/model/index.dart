3730dd7a10c5e97454442c6815423de7fe0559ce
Yasin Ertekin <yasinertekinn@outlook.com>
2024-04-13 23:56:33 +0300

    Add horoscope and prompt features to model/index.dart

--- package/gen/lib/src/model/index.dart
+++ package/gen/lib/src/model/index.dart
@@ -1,3 +1,7 @@
+export 'horoscope/horoscope_model.dart';
+export 'lunar_tips/lunar_tips.dart';
 export 'onboarding/onboarding.dart';
+export 'prompt/prompt.dart';
+export 'prompt_response/prompt_response.dart';
 export 'users/users.dart';
 export 'weather/weather.dart';
