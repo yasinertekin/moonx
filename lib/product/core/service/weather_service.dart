7a8ee20a6d66d34bb626630c8b5bf84c37358bec
Yasin Ertekin <yasinertekinn@outlook.com>
2024-04-13 12:19:53 +0300

    Add WeatherService class for fetching weather data

---
+++ lib/product/core/service/weather_service.dart
@@ -0,0 +1,24 @@
+import 'package:dio/dio.dart';
+import 'package:gen/gen.dart';
+import 'package:moonx/product/core/initialize/config/app_environment.dart';
+
+abstract final class IWeatherService {
+  IWeatherService({required this.service});
+  Future<Weather> fetchWeather(String cityName);
+  final Dio service;
+}
+
+final class WeatherServiceImpl extends IWeatherService {
+  WeatherServiceImpl({required super.service});
+  @override
+  Future<Weather> fetchWeather(String cityName) async {
+    final response = await service.get<Map<String, dynamic>>(
+      'current.json?key=${AppEnvironmentItems.weatherApiKey}&q=$cityName&aqi=no',
+    );
+    if (response.statusCode == 200) {
+      return Weather.fromJson(response.data!);
+    } else {
+      throw Exception('Failed to load weather');
+    }
+  }
+}
