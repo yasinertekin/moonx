f7a36f1d2772a13ca3a68ef46dd4c5bf397382db
Yasin Ertekin <yasinertekinn@outlook.com>
2024-04-13 12:18:38 +0300

    Add LoadingWidget to SplashView

---
+++ lib/feature/splash/view/widget/loading_widget.dart
@@ -0,0 +1,12 @@
+part of '../splash_view.dart';
+
+final class _LoadingWidget extends StatelessWidget {
+  const _LoadingWidget();
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: CircularProgressIndicator(),
+    );
+  }
+}
