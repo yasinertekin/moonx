b349a27040703e7286e959a46f280e7e43494785
Yasin Ertekin <yasinertekinn@outlook.com>
2024-04-13 12:16:23 +0300

    Add HomeCalenderWidget to home view

---
+++ lib/feature/home/view/widget/home_calender_widget.dart
@@ -0,0 +1,18 @@
+part of '../home_view.dart';
+
+final class _HomeCalenderWidget extends StatelessWidget {
+  const _HomeCalenderWidget();
+
+  @override
+  Widget build(BuildContext context) {
+    return TableCalendar<DateTime>(
+      firstDay: DateTime.utc(2022, 10, 16),
+      lastDay: DateTime.utc(2030, 3, 14),
+      focusedDay: DateTime.now(),
+      headerStyle: const HeaderStyle(
+        titleCentered: true,
+        formatButtonVisible: false,
+      ),
+    );
+  }
+}
