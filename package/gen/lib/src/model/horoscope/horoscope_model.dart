21c4e7d2cda6ad9328af4ae16d0e13eb2b9c32c1
Yasin Ertekin <yasinertekinn@outlook.com>
2024-04-13 23:56:45 +0300

    Add HoroscopeModel class to handle horoscope data

---
+++ package/gen/lib/src/model/horoscope/horoscope_model.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_svg/flutter_svg.dart';
+
+final class HoroscopeModel {
+  final String name;
+  final SvgPicture image;
+
+  const HoroscopeModel({required this.name, required this.image});
+}
