a12394df01b29a2e4066d9789cf893bf00b9bbc2
Yasin Ertekin <yasinertekinn@outlook.com>
2024-04-13 12:19:34 +0300

    Add DioManager class for managing Dio instance

---
+++ lib/product/core/manager/dio_manager.dart
@@ -0,0 +1,16 @@
+import 'package:dio/dio.dart';
+
+final class DioManager {
+  factory DioManager({required String baseUrl}) {
+    _singleton._dio.options = BaseOptions(baseUrl: baseUrl);
+    return _singleton;
+  }
+
+  DioManager._internal();
+  static final DioManager _singleton = DioManager._internal();
+
+  final Dio _dio = Dio();
+
+  /// Get the Dio instance
+  Dio get dio => _dio;
+}
