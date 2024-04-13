5c3ed126558eb332675148fdff961527e35a6ccd
Yasin Ertekin <yasinertekinn@outlook.com>
2024-04-13 12:15:19 +0300

    Add locator.dart file for dependency injection

---
+++ lib/locator.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+import 'package:moonx/product/utils/cache/users_bloc.dart';
+import 'package:moonx/product/utils/router/app_router.dart';
+
+@immutable
+abstract final class Locator {
+  static final GetIt _getIt = GetIt.instance;
+
+  /// Setup the locator
+  static Future<void> setup() async {
+    _getIt
+      ..registerSingleton<UsersBloc>(UsersBloc())
+      ..registerSingleton<AppRouter>(AppRouter());
+  }
+
+  static UsersBloc get usersBloc => _getIt<UsersBloc>();
+  static AppRouter get appRouter => _getIt<AppRouter>();
+}
