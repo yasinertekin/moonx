39f56f2fbd133e1dfd49a6350578fb991afd6cfd
Yasin Ertekin <yasinertekinn@outlook.com>
2024-04-14 21:17:17 +0300

    Refactor project_button_mixin.dart to use appRouter for navigation

--- lib/feature/onboarding/view/mixin/project_button_mixin.dart
+++ lib/feature/onboarding/view/mixin/project_button_mixin.dart
@@ -22,12 +22,7 @@ base mixin _ProjectButtonMixin on State<_OnboardingButton> {
         context.read<OnboardingCubit>().nextPage();
       case OnBoardingEnum.birthPlace:
         if (placeOfBirth.isEmpty) return;
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (_) => const HomeView(),
-          ),
-        );
+        Locator.appRouter.push(const HomeRoute());
     }
   }
 }
