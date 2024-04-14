a3ccb1b3b6023b19c32c73bc2576dce8b8f46ac3
Yasin Ertekin <yasinertekinn@outlook.com>
2024-04-14 21:17:23 +0300

    Add new string constants for home view in string_constants.dart

--- lib/product/core/constants/string_constants.dart
+++ lib/product/core/constants/string_constants.dart
@@ -6,6 +6,24 @@ import 'package:flutter/material.dart';
 final class StringConstants {
   const StringConstants._();
 
+  /// Home View
+  ///
+  /// Home Banner
+  static const String whatIsForToday = 'What is for Today?';
+  static const String yourDailyHoroscope = 'Your Daily Horoscope';
+
+  /// Home Header View
+  ///
+  /// Location Text
+  static const String loadingLocationInformation =
+      'Loading Location Information...';
+  static const String locationPermissionDenied = 'Location Permission Denied';
+  static const String locationNotFound = 'Location Not Found';
+
+  /// Time Based Text
+  static const String goodMorning = 'Good Morning!';
+  static const String goodEvening = 'Good Evening!';
+
   /// Project Button
   static const String next = 'Next';
 }
