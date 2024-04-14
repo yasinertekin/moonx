64d2866c9f246ec979a9149ac1a69657e8b16649
Yasin Ertekin <yasinertekinn@outlook.com>
2024-04-14 21:14:03 +0300

    Add _DailyHoroscope widget to display daily horoscope in home view

---
+++ lib/feature/home/view/widget/daily_horoscope/daily_horoscope.dart
@@ -0,0 +1,83 @@
+part of '../../home_view.dart';
+
+final class _DailyHoroscope extends StatefulWidget {
+  const _DailyHoroscope();
+
+  @override
+  State<_DailyHoroscope> createState() => _DailyHoroscopeState();
+}
+
+final class _DailyHoroscopeState extends State<_DailyHoroscope> {
+  @override
+  void initState() {
+    super.initState();
+    context.read<DailyHoroscopeCubit>().generateDailyHoroscope(
+          users: context.read<UsersBloc>().state.users,
+        );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<DailyHoroscopeCubit, DailyHoroscopeState>(
+      builder: (context, state) {
+        switch (state.status) {
+          case GeminiStatus.loading:
+            return const CustomProgressIndicator();
+          case GeminiStatus.success:
+            return _DailyHoroscopeResult(
+              promptResponse: state.promptResponse!,
+            );
+          case GeminiStatus.failure:
+            return Text(
+              state.errorMessage,
+            );
+          case GeminiStatus.initial:
+            return const SizedBox.shrink();
+        }
+      },
+    );
+  }
+}
+
+final class _DailyHoroscopeResult extends StatelessWidget {
+  const _DailyHoroscopeResult({
+    required this.promptResponse,
+  });
+
+  final PromptResponse promptResponse;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: ProjectPadding.low.paddingAll,
+      child: Material(
+        shape: RoundedRectangleBorder(
+          borderRadius: ProjectRadius.medium.borderRadius,
+        ),
+        child: Padding(
+          padding: ProjectPadding.medium.paddingAll,
+          child: _HoroscopeResultText(promptResponse: promptResponse),
+        ),
+      ),
+    );
+  }
+}
+
+final class _HoroscopeResultText extends StatelessWidget {
+  const _HoroscopeResultText({
+    required this.promptResponse,
+  });
+
+  final PromptResponse promptResponse;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      promptResponse.text ?? '',
+      style: context.theme.textTheme.bodyLarge?.copyWith(
+        color: ColorName.colorEmptiness,
+        fontSize: 14,
+      ),
+    );
+  }
+}
