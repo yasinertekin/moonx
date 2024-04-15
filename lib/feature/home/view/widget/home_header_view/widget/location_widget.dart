9193a893e735b6df4691c3dacd877340720a8b04
Yasin Ertekin <yasinertekin24@gmail.com>
2024-04-15 17:54:54 +0300

    Add HourlyForecastList and HourlyForecastItem widgets for displaying hourly weather forecast

--- lib/feature/home/view/widget/home_header_view/widget/location_widget.dart
+++ lib/feature/home/view/widget/home_header_view/widget/location_widget.dart
@@ -19,7 +19,72 @@ final class _LocationWidget extends StatelessWidget {
         const _DateTimeText(),
         const EmptySizedBox(),
         const _WeatherText(),
+        const EmptySizedBox(),
+        const _HourlyForecastList(),
       ],
+    );
+  }
+}
+
+final class _HourlyForecastList extends StatelessWidget {
+  const _HourlyForecastList();
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 50,
+      width: context.dynamicWidth(1),
+      child: ListView.separated(
+        separatorBuilder: (BuildContext context, int index) {
+          return EmptySizedBox(
+            width: context.dynamicWidth(0.047),
+          );
+        },
+        physics: const NeverScrollableScrollPhysics(),
+        scrollDirection: Axis.horizontal,
+        itemCount: HourlyWeatherForecast.hourlyWeatherForecastList.length,
+        itemBuilder: (BuildContext context, int index) {
+          final hourlyWeatherForecast =
+              HourlyWeatherForecast.hourlyWeatherForecastList[index];
+          return _HourlyForecastItem(
+            hourlyWeatherForecast: hourlyWeatherForecast,
+          );
+        },
+      ),
+    );
+  }
+}
+
+final class _HourlyForecastItem extends StatelessWidget {
+  const _HourlyForecastItem({
+    required this.hourlyWeatherForecast,
+  });
+
+  final HourlyWeatherForecast hourlyWeatherForecast;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 50,
+      width: 83,
+      child: Material(
+        shape: RoundedRectangleBorder(
+          borderRadius: ProjectRadius.small.borderRadius,
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            hourlyWeatherForecast.icon,
+            Text(
+              hourlyWeatherForecast.time,
+              style: context.theme.textTheme.bodyLarge!.copyWith(
+                color: Colors.white,
+                fontSize: 12,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
