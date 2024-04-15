c61ac2720a4653ac2078c13cad92ca6a37e5111f
Yasin Ertekin <yasinertekin24@gmail.com>
2024-04-15 17:54:12 +0300

    Refactor DailyHoroscopeState class to include isLoading, errorMessage, promptResponse, and status properties

--- lib/feature/home/cubit/daily_horoscope/daily_horoscope_state.dart
+++ lib/feature/home/cubit/daily_horoscope/daily_horoscope_state.dart
@@ -1,20 +1,29 @@
 part of 'dialy_horoscope_cubit.dart';
 
+/// Represents the state of the daily horoscope feature.
 final class DailyHoroscopeState extends Equatable {
+  /// Constructs a [DailyHoroscopeState] instance.
   const DailyHoroscopeState({
     this.isLoading = false,
     this.errorMessage = '',
     this.promptResponse,
     this.status = GeminiStatus.initial,
   });
+
+  /// Indicates whether the daily horoscope is currently being loaded.
   final bool isLoading;
 
+  /// The error message associated with the daily horoscope.
   final String errorMessage;
 
+  /// The response from the prompt associated with the daily horoscope.
   final PromptResponse? promptResponse;
 
+  /// The status of the daily horoscope.
   final GeminiStatus status;
 
+  /// Creates a copy of
+  ///  this [DailyHoroscopeState] instance with the given parameters overridden.
   DailyHoroscopeState copyWith({
     bool? isLoading,
     String? errorMessage,
@@ -33,9 +42,18 @@ final class DailyHoroscopeState extends Equatable {
   List<Object?> get props => [isLoading, errorMessage, promptResponse, status];
 }
 
+/// Represents the status of the daily horoscope.
+/// Enum representing the status of a Gemini horoscope.
 enum GeminiStatus {
+  /// The initial status before any data is loaded.
   initial,
+
+  /// The status when data is being loaded.
   loading,
+
+  /// The status when data is successfully loaded.
   success,
+
+  /// The status when there is a failure in loading data.
   failure,
 }
