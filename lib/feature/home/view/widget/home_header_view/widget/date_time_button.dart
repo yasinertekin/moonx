b207367a626e08d21ffcbf08bd55904f5b90ac5c
Yasin Ertekin <yasinertekin24@gmail.com>
2024-04-15 17:54:48 +0300

    Refactor _DateTimeButton widget to use BlocBuilder for dynamic button behavior

--- lib/feature/home/view/widget/home_header_view/widget/date_time_button.dart
+++ lib/feature/home/view/widget/home_header_view/widget/date_time_button.dart
@@ -1,11 +1,7 @@
 part of '../../../home_view.dart';
 
 final class _DateTimeButton extends StatelessWidget {
-  const _DateTimeButton(
-    this.pageController,
-  );
-
-  final PageController pageController;
+  const _DateTimeButton();
 
   @override
   Widget build(BuildContext context) {
@@ -13,33 +9,26 @@ final class _DateTimeButton extends StatelessWidget {
       alignment: Alignment.topRight,
       child: Padding(
         padding: ProjectPadding.medium.paddingOnlyRight,
-        child: ProjectButton(
-          backgroundColor: ColorName.colorKon,
-          width: context.dynamicWidth(0.3),
-          height: context.dynamicHeight(0.05),
-          borderRadiusGeometry: ProjectRadius.small.borderRadius,
-          onPressed: _changePage,
-          title: '2 Aug 2023',
-          titleColor: ColorName.colorThickBlue,
+        child: BlocBuilder<HomeHeaderCubit, HomeHeaderState>(
+          builder: (context, state) {
+            return ProjectButton(
+              backgroundColor: ColorName.colorKon,
+              width: context.dynamicWidth(0.3),
+              height: context.dynamicHeight(0.05),
+              borderRadiusGeometry: ProjectRadius.small.borderRadius,
+              onPressed: () {
+                if (state.index == HomeHeader.location) {
+                  context.read<HomeHeaderCubit>().changePage(1);
+                } else {
+                  context.read<HomeHeaderCubit>().changePage(0);
+                }
+              },
+              title: '2 Aug 2023',
+              titleColor: ColorName.colorThickBlue,
+            );
+          },
         ),
       ),
     );
-  }
-
-  void _changePage() {
-    switch (_HomeHeader.values[pageController.page!.toInt()]) {
-      case _HomeHeader.location:
-        pageController.animateToPage(
-          _HomeHeader.values.length - 1,
-          duration: const Duration(milliseconds: 500),
-          curve: Curves.ease,
-        );
-      case _HomeHeader.calendar:
-        pageController.animateToPage(
-          _HomeHeader.values.length - 2,
-          duration: const Duration(milliseconds: 500),
-          curve: Curves.ease,
-        );
-    }
   }
 }
