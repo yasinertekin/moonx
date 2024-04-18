cdeb84c0b400ab3ac4fd9a2c4c84a1a5ef898832
Yasin Ertekin <yasinertekin24@gmail.com>
2024-04-18 23:40:58 +0300

    Add NowPlayingBar and NowPlayingSlideBar widgets to ProfileView

--- lib/feature/profile/view/profile_view.dart
+++ lib/feature/profile/view/profile_view.dart
@@ -1,5 +1,7 @@
 import 'package:auto_route/annotations.dart';
 import 'package:flutter/material.dart';
+import 'package:moonx/product/widget/now_playing/now_playing_bar.dart';
+import 'package:moonx/product/widget/now_playing/now_playing_slide_bar.dart';
 
 @RoutePage()
 final class ProfileView extends StatelessWidget {
@@ -7,6 +9,15 @@ final class ProfileView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          Text('data'),
+          NowPlayingBar(),
+          NowPlayingSlideBar(),
+        ],
+      ),
+    );
   }
 }
