b9ceacfc0ac47253ab3f6403e1bba3bb677dbcee
Yasin Ertekin <yasinertekin24@gmail.com>
2024-04-18 23:39:45 +0300

    Refactor meditation tab bar view and app bar in MeditationView

--- lib/feature/meditation/view/widget/medidation_tab_bar.dart
+++ lib/feature/meditation/view/widget/medidation_tab_bar.dart
@@ -1,9 +1,8 @@
 part of '../medidation_view.dart';
 
-class _MedidationTabbar extends StatelessWidget {
+final class _MedidationTabbar extends StatelessWidget {
   const _MedidationTabbar({
     required this.tabsRouter,
-    super.key,
   });
 
   final TabsRouter tabsRouter;
@@ -17,38 +16,88 @@ class _MedidationTabbar extends StatelessWidget {
         shape: RoundedRectangleBorder(
           borderRadius: ProjectRadius.small.borderRadius,
         ),
-        child: Padding(
-          padding: const EdgeInsets.all(8),
-          child: SizedBox(
-            height: context.dynamicHeight(0.05),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: List.generate(
-                MedidationTabs.values.length,
-                (index) {
-                  final tab = MedidationTabs.values[index];
-                  return Expanded(
-                    child: Material(
-                      color: tabsRouter.activeIndex == index
-                          ? ColorName.colorThickBlue
-                          : ColorName.colorAtlanticCharter,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: ProjectRadius.small.borderRadius,
-                      ),
-                      child: GestureDetector(
-                        onTap: () async {
-                          tabsRouter.setActiveIndex(index);
-                        },
-                        child: Center(
-                          child: Text(tab.title),
-                        ),
-                      ),
-                    ),
-                  );
-                },
-              ),
-            ),
-          ),
+        child: _TabBarList(tabsRouter: tabsRouter),
+      ),
+    );
+  }
+}
+
+final class _TabBarList extends StatelessWidget {
+  const _TabBarList({
+    required this.tabsRouter,
+  });
+
+  final TabsRouter tabsRouter;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: context.dynamicHeight(0.05),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: List.generate(
+          MedidationTabs.values.length,
+          (index) {
+            final tab = MedidationTabs.values[index];
+            return _TabBarView(
+              tabsRouter: tabsRouter,
+              tab: tab,
+              index: index,
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+final class _TabBarView extends StatelessWidget {
+  const _TabBarView({
+    required this.tabsRouter,
+    required this.tab,
+    required this.index,
+  });
+
+  final TabsRouter tabsRouter;
+  final MedidationTabs tab;
+  final int index;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Material(
+        color: tabsRouter.activeIndex == index
+            ? ColorName.colorThickBlue
+            : ColorName.colorAtlanticCharter,
+        shape: RoundedRectangleBorder(
+          borderRadius: ProjectRadius.small.borderRadius,
+        ),
+        child: GestureDetector(
+          onTap: () async {
+            tabsRouter.setActiveIndex(index);
+          },
+          child: _TabTitle(tab: tab),
+        ),
+      ),
+    );
+  }
+}
+
+final class _TabTitle extends StatelessWidget {
+  const _TabTitle({
+    required this.tab,
+  });
+
+  final MedidationTabs tab;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        tab.title,
+        style: context.theme.textTheme.titleLarge!.copyWith(
+          color: Colors.white,
+          fontSize: 13,
         ),
       ),
     );
