9eabc589607ebd040ced2e7b9843e4329837722b
Yasin Ertekin <yasinertekin24@gmail.com>
2024-04-18 23:40:15 +0300

    Add _SoundItemList widget to MeditationView

---
+++ lib/feature/meditation/view/widget/sound_item_list.dart
@@ -0,0 +1,51 @@
+part of 'medidations_custom_view.dart';
+
+final class _SoundItemList extends StatelessWidget {
+  const _SoundItemList({
+    required this.index,
+    required this.soundLists,
+  });
+
+  final int index;
+  final SoundCategory soundLists;
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocSelector<SoundCubit, SoundState, List<Sound>>(
+      selector: (state) {
+        if (soundLists == SoundLists.medidation) {
+          return state.medidationlist[index].sound;
+        } else if (soundLists == SoundLists.yoga) {
+          return state.sleepList[index].sound;
+        } else {
+          return state.musicList[index].sound;
+        }
+      },
+      builder: (context, state) {
+        return SizedBox(
+          height: context.dynamicHeight(
+            0.15 * state.length,
+          ),
+          child: GridView.builder(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              crossAxisSpacing: 10,
+              mainAxisSpacing: 10,
+              mainAxisExtent: 250,
+            ),
+            physics: const NeverScrollableScrollPhysics(),
+            itemCount: state.length,
+            itemBuilder: (BuildContext context, int innerIndex) {
+              final sound = state[innerIndex];
+              return _SoundItem(
+                sound: sound,
+                index: innerIndex,
+                soundList: soundLists,
+              );
+            },
+          ),
+        );
+      },
+    );
+  }
+}
