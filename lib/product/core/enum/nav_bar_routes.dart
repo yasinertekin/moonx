2e44306f348a28155433ac4f3ac4dc825a5aafd9
Yasin Ertekin <yasinertekin24@gmail.com>
2024-04-19 17:01:53 +0300

    Refactor NavBarView to use StringConstants for label texts

--- lib/product/core/enum/nav_bar_routes.dart
+++ lib/product/core/enum/nav_bar_routes.dart
@@ -1,14 +1,24 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:moonx/product/utils/router/app_router.gr.dart';
 
+/// Enum representing the routes for the navigation bar.
 enum NavbarRoutes {
+  /// Represents the home route.
   home(HomeRoute()),
+
+  /// Represents the meditation route.
   meditation(MeditationRoute()),
+
+  /// Represents the profile route.
   profile(
     ProfileRoute(),
   );
 
+  /// The page associated with the route.
   final PageRouteInfo page;
 
+  /// Constructs a [NavbarRoutes] with the given [page].
+  ///
+  // ignore: sort_constructors_first
   const NavbarRoutes(this.page);
 }
