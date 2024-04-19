3bf875e36d527f330c11ef520c2882104360f0b1
Yasin Ertekin <yasinertekin24@gmail.com>
2024-04-19 17:01:33 +0300

    Refactor StringConstants class to add new constant strings and update existing ones

--- lib/product/core/constants/string_constants.dart
+++ lib/product/core/constants/string_constants.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 @immutable
 
 /// String Constants
+///
+/// This class contains constant strings used throughout the application.
 final class StringConstants {
   const StringConstants._();
 
@@ -10,6 +12,8 @@ final class StringConstants {
   ///
   /// Home Banner
   static const String whatIsForToday = 'What is for Today?';
+
+  /// Home Header
   static const String yourDailyHoroscope = 'Your Daily Horoscope';
 
   /// Home Header View
@@ -17,13 +21,55 @@ final class StringConstants {
   /// Location Text
   static const String loadingLocationInformation =
       'Loading Location Information...';
+
+  /// Location Permission Denied
   static const String locationPermissionDenied = 'Location Permission Denied';
+
+  /// Location Not Found
   static const String locationNotFound = 'Location Not Found';
+
+  /// Lunar Tips
+  static const String lunarTips = 'Lunar Tips';
+
+  /// Location Error
+  static const String locationError = 'Location Error';
 
   /// Time Based Text
   static const String goodMorning = 'Good Morning!';
+
+  /// Good Evening
   static const String goodEvening = 'Good Evening!';
 
   /// Project Button
   static const String next = 'Next';
+
+  /// Onboarding View
+  static const String pleaseSelectBirthDateAndTime =
+      'Please select birth date and time';
+
+  /// Nav Bar View
+  ///
+  static const String profileLabel = 'Profile';
+
+  /// Meditation Label
+  static const String meditationLabel = 'Meditation';
+
+  /// Home Label
+  static const String homeLabel = 'Home';
+
+  /// Profile
+  static const String settingsTitle = 'Settings';
+
+  /// Banner Description
+  static const String bannerDescription =
+      'Unlock package in order to access the features';
+
+  /// Get Premium
+  static const String getPremium = 'Get Premium';
+
+  /// Unlock All Contents
+  static const String toAccessAllContents = 'to Access All Contents!';
+
+  /// Gemini Pro
+  static const String geminiPro = 'gemini-pro';
 }
