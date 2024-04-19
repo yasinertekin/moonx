3667ce61cd52cb372076ad88cc414e9255de75a5
Yasin Ertekin <yasinertekin24@gmail.com>
2024-04-19 17:03:36 +0300

    Fix typo in CustomProgressIndicator class documentation

--- lib/product/widget/loading/custom_progress_indicator.dart
+++ lib/product/widget/loading/custom_progress_indicator.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 
+/// A custom progress indicator widget.
+///
+/// This widget displays a circular progress indicator
+///  at the center of the screen.
 final class CustomProgressIndicator extends StatelessWidget {
+  /// Constructs a [CustomProgressIndicator] object.
   const CustomProgressIndicator({super.key});
 
   @override
