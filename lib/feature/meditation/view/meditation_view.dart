b8bbb69a6a3d29dcccdd6a8a2183737fb2f43f23
Yasin Ertekin <yasinertekin24@gmail.com>
2024-04-19 16:59:18 +0300

    Refactor meditation_view.dart and related files, update imports and class names

--- lib/feature/meditation/view/meditation_view.dart
+++ lib/feature/meditation/view/meditation_view.dart
@@ -1,7 +1,7 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:gen/gen.dart';
-import 'package:moonx/feature/meditation/cubit/medidation_cubit.dart';
+import 'package:moonx/product/core/enum/meditation_tabs.dart';
 import 'package:moonx/product/core/enum/project_padding.dart';
 import 'package:moonx/product/core/enum/project_radius.dart';
 import 'package:moonx/product/core/extension/build_context_extension.dart';
@@ -9,22 +9,25 @@ import 'package:moonx/product/utils/router/app_router.gr.dart';
 import 'package:moonx/product/widget/now_playing/now_playing_bar.dart';
 import 'package:moonx/product/widget/now_playing/now_playing_slide_bar.dart';
 
-part 'widget/medidation_tab_bar.dart';
-part 'widget/medidation_tab_bar_view.dart';
-part 'widget/medidation_view_background_image.dart';
+part 'widget/meditation_tab_bar.dart';
+part 'widget/meditation_tab_bar_view.dart';
+part 'widget/meditation_view_background_image.dart';
 
 @RoutePage()
+
+/// The view for the meditation screen.
 final class MeditationView extends StatelessWidget {
+  /// Constructs a [MeditationView] object.
   const MeditationView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const _MedidationScaffold();
+    return const _MeditationScaffold();
   }
 }
 
-final class _MedidationScaffold extends StatelessWidget {
-  const _MedidationScaffold();
+final class _MeditationScaffold extends StatelessWidget {
+  const _MeditationScaffold();
 
   @override
   Widget build(BuildContext context) {
@@ -32,8 +35,8 @@ final class _MedidationScaffold extends StatelessWidget {
       body: Stack(
         fit: StackFit.expand,
         children: [
-          _MedidationViewBackgroundImage(),
-          _MedidationTabBarView(),
+          _MeditationDetailBackgroundImage(),
+          _MeditationTabBarView(),
           NowPlayingBar(),
           NowPlayingSlideBar(),
         ],
