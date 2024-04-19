bb139b09f0630931cb3cb2415d605d4fa46da432
Yasin Ertekin <yasinertekin24@gmail.com>
2024-04-19 16:57:21 +0300

    Refactor Weather class in weather.dart to improve code readability and documentation

--- package/gen/lib/src/model/weather/weather.dart
+++ package/gen/lib/src/model/weather/weather.dart
@@ -1,40 +1,39 @@
 import 'package:equatable/equatable.dart';
 import 'package:flutter/widgets.dart';
+import 'package:gen/src/model/weather/current.dart';
 import 'package:gen/src/model/weather/weather_location.dart';
 import 'package:json_annotation/json_annotation.dart';
 
-import 'current.dart';
-
 part 'weather.g.dart';
 
+/// Represents the weather information.
 @JsonSerializable()
 @immutable
 final class Weather with EquatableMixin {
-  @JsonKey(name: 'location')
-  final WeatherLocation? location;
-  @JsonKey(name: 'current')
-  final Current? current;
-
+  /// Constructs a [Weather] instance.
+  ///
+  /// [location] represents the location information.
+  /// [current] represents the current weather information.
   Weather({
     this.location,
     this.current,
   });
 
+  /// Constructs a [Weather] instance from a JSON map.
   factory Weather.fromJson(Map<String, dynamic> json) =>
       _$WeatherFromJson(json);
 
+  /// Represents the location information.
+  @JsonKey(name: 'location')
+  final WeatherLocation? location;
+
+  /// Represents the current weather information.
+  @JsonKey(name: 'current')
+  final Current? current;
+
+  /// Converts the [Weather] instance to a JSON map.
   Map<String, dynamic> toJson() => _$WeatherToJson(this);
 
   @override
   List<Object?> get props => [location, current];
-
-  Weather copyWith({
-    WeatherLocation? location,
-    Current? current,
-  }) {
-    return Weather(
-      location: location ?? this.location,
-      current: current ?? this.current,
-    );
-  }
 }
