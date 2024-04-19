2754848def9c16c4c77e2c270d09f8116e541a8d
Yasin Ertekin <yasinertekin24@gmail.com>
2024-04-19 11:51:22 +0300

    Add profile app bar widget

---
+++ lib/feature/profile/view/widget/profile_app_bar.dart
@@ -0,0 +1,45 @@
+part of '../profile_view.dart';
+
+final class _ProfileAppBar extends StatelessWidget
+    implements PreferredSizeWidget {
+  const _ProfileAppBar();
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: ColorName.colorRavensCoat,
+      leading: const _AppBarLeading(),
+      title: const _AppBarTitle(),
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kTextTabBarHeight);
+}
+
+final class _AppBarLeading extends StatelessWidget {
+  const _AppBarLeading();
+
+  @override
+  Widget build(BuildContext context) {
+    return Assets.images.imgRightBtn.image(
+      package: 'gen',
+    );
+  }
+}
+
+final class _AppBarTitle extends StatelessWidget {
+  const _AppBarTitle();
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      'Settings',
+      style: context.theme.textTheme.titleLarge?.copyWith(
+        color: ColorName.colorEmptiness,
+        fontWeight: FontWeight.bold,
+        fontSize: 18,
+      ),
+    );
+  }
+}
