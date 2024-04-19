183dfe0773691da4750b731d7cd1548e6f418ac8
Yasin Ertekin <yasinertekin24@gmail.com>
2024-04-19 17:03:06 +0300

    Add formatTimer method to FormatTimerMixin for formatting timer durations

--- lib/product/utils/mixin/format_timer.dart
+++ lib/product/utils/mixin/format_timer.dart
@@ -1,5 +1,13 @@
+/// A mixin that provides a method to format
+///  a [Duration] into a string representation of a timer.
 base mixin FormatTimerMixin {
+  /// Formats the given [timer] duration into
+  /// a string representation of a timer.
+  ///
+  /// The returned string has the format '
+  /// mm:ss', where 'mm' represents the minutes and 'ss' represents the seconds.
   String formatTimer(Duration timer) {
+    // ignore: lines_longer_than_80_chars
     return '${(timer.inSeconds ~/ 60).toString().padLeft(2, '0')}:${(timer.inSeconds % 60).toString().padLeft(2, '0')}';
   }
 }
