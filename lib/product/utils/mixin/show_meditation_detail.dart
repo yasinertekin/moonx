2456517ca263df354b51d53e2fcd3e56f0321a8e
Yasin Ertekin <yasinertekin24@gmail.com>
2024-04-19 17:03:17 +0300

    Add ShowMeditationDetailMixin for showing the details of a meditation

---
+++ lib/product/utils/mixin/show_meditation_detail.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:gen/gen.dart';
+import 'package:moonx/feature/meditation_detail/meditation_detail_view.dart';
+
+/// A mixin that provides a method to show the details of a meditation.
+base mixin ShowMeditationDetailMixin {
+  /// Shows the details of a meditation in a modal bottom sheet.
+  ///
+  /// The [context] parameter is the build context.
+  /// The [sound] parameter is the sound object representing the meditation.
+  void showMeditationDetail(BuildContext context, Sound sound) {
+    showModalBottomSheet<void>(
+      isScrollControlled: true,
+      context: context,
+      builder: (BuildContext context) {
+        return MeditationDetailView(
+          sound: sound,
+        );
+      },
+    );
+  }
+}
