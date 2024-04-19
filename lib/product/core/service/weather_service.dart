c217a09b1f401a5b961354b5f64664c3640a9fea
Yasin Ertekin <yasinertekin24@gmail.com>
2024-04-19 17:02:54 +0300

    Refactor weather service implementation and add documentation

--- lib/product/core/service/weather_service.dart
+++ lib/product/core/service/weather_service.dart
@@ -1,21 +1,38 @@
+import 'dart:io';
+
 import 'package:dio/dio.dart';
 import 'package:gen/gen.dart';
-import 'package:moonx/product/core/initialize/config/app_environment.dart';
+import 'package:moonx/product/core/enum/network_url.dart';
 
+/// An abstract class representing a weather service.
 abstract final class IWeatherService {
+  /// Constructs a [IWeatherService] instance with the given [service].
   IWeatherService({required this.service});
+
+  /// Fetches the weather for the given [cityName].
+  ///
+  /// Returns a [Future] that completes with a [Weather] object.
   Future<Weather> fetchWeather(String cityName);
+
+  /// The Dio service used for making HTTP requests.
   final Dio service;
 }
 
+/// Implementation of the [IWeatherService] interface.
 final class WeatherServiceImpl extends IWeatherService {
+  /// Constructs a [WeatherServiceImpl] instance with the given [service].
   WeatherServiceImpl({required super.service});
+
+  /// Fetches the weather for the given [cityName].
+  ///
+  /// Returns a [Future] that completes with a [Weather] object.
+  /// Throws an [Exception] if the weather data fails to load.
   @override
   Future<Weather> fetchWeather(String cityName) async {
     final response = await service.get<Map<String, dynamic>>(
-      'current.json?key=${AppEnvironmentItems.weatherApiKey.value}&q=$cityName&aqi=no',
+      NetworkUrl.weatherApiEndPoint.getValue(cityName),
     );
-    if (response.statusCode == 200) {
+    if (response.statusCode == HttpStatus.ok) {
       return Weather.fromJson(response.data!);
     } else {
       throw Exception('Failed to load weather');
