c928190de0ad2a8cd4d2b956e8e7acd174b6220a
Yasin Ertekin <yasinertekin24@gmail.com>
2024-04-19 17:01:44 +0300

    Refactor Month enum to include comments for each month

--- lib/product/core/enum/month.dart
+++ lib/product/core/enum/month.dart
@@ -1,14 +1,38 @@
+/// Represents the months of the year.
 enum Month {
+  /// January.
   january,
+
+  /// February.
   february,
+
+  /// March.
   march,
+
+  /// April.
   april,
+
+  /// May.
   may,
+
+  /// June.
   june,
+
+  /// July.
   july,
+
+  /// August.
   august,
+
+  /// September.
   september,
+
+  /// October.
   october,
+
+  /// November.
   november,
+
+  /// December.
   december;
 }
