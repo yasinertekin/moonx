9d0968cf670a2606d7a911e5924fe9d5e2fb2a20
Yasin Ertekin <yasinertekin24@gmail.com>
2024-04-19 17:02:29 +0300

    Refactor DioManager class to use factory constructor and add documentation

--- lib/product/core/manager/dio_manager.dart
+++ lib/product/core/manager/dio_manager.dart
@@ -2,17 +2,21 @@ import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
 @immutable
+
+/// A singleton class that manages the Dio instance for making HTTP requests.
 final class DioManager {
+  /// Factory constructor for the DioManager.
   factory DioManager({required String baseUrl}) {
     _singleton._dio.options = BaseOptions(baseUrl: baseUrl);
     return _singleton;
   }
 
   DioManager._internal();
+
   static final DioManager _singleton = DioManager._internal();
 
   final Dio _dio = Dio();
 
-  /// Get the Dio instance
+  /// Get the Dio instance.
   Dio get dio => _dio;
 }
